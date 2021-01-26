190782b2645a4575900296211d186f38eadc4201
taecolaza2541 <77877288+taecolaza2541@users.noreply.github.com>
2021-01-26 22:11:25 +0700

    Send Assignment Week1

---
+++ Gi455unity/Assets/Apply.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Apply : MonoBehaviour
+{
+    public Text text;
+    public InputField Inputtext;
+    private string box;
+
+    public void buttons()
+    {
+        switch (box)
+        {
+            case "Line":
+                text.text = box + " is found ";
+                break;
+            case "IG":
+                text.text = box + " is found ";
+                break;
+            case "Facebook":
+                text.text = box + " is found ";
+                break;
+            case "Tinder":
+                text.text = box + " is found ";
+                break;
+            case "Twitter":
+                text.text = box + " is found ";
+                break;
+            default:
+                text.text = box + " is not found ";
+                break;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+            box = Inputtext.text; 
+                }
+}
